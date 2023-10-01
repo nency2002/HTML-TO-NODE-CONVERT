4185fcaae0174ec21ccd98ba7291c5200f0d6268
nency kotadiya <kotadiyanency7@gmail.com>
2023-10-01 12:13:35 +0530

    pr

---
+++ recipe/middle.js
@@ -0,0 +1,17 @@
+const check = (req, res, next) => {
+
+  let { name, description, preparationTime, cookingTime, imageUrl, country, veg } = req.body
+
+  if (name && description && preparationTime && cookingTime && imageUrl && country && veg) {
+    next()
+  }
+  else {
+    res.status(400).send("All fields are required.");
+  }
+
+}
+
+module.exports = check;
+
+
+
